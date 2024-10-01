7fee33c7f88c6981df84c222f7fbab6ef8271428
fedeval <federico.valenza5@gmail.com>
2024-10-01 11:37:54 +0300

    Concurrency [WIP]

--- 09-concurrency/main.go
+++ 09-concurrency/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println(runtime.NumCPU())
+	fmt.Println(runtime.NumGoroutine())
+
+	go foo() // will run in a separate "thread"/goroutine
+	bar()
+
+	fmt.Println(runtime.NumCPU())
+	fmt.Println(runtime.NumGoroutine())
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+}
+
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
